cd397b3fccd6f53da2a93d8fe9b053ea39670da9
Adidya <Adidyadarmawan12@gmail.com>
2026-04-02 10:30:16 +0800

    Initial Commit

---
+++ todo2/todo2App.swift
@@ -0,0 +1,17 @@
+//
+//  todo2App.swift
+//  todo2
+//
+//  Created by 17 on 2026/4/2.
+//
+
+import SwiftUI
+
+@main
+struct todo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
